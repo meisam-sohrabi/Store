25d8f2746edd799f5ac6664c0ac5900541c72b36
meisam-sohrabi <meisam.sohrabi2@gmail.com>
2025-10-15 12:32:34 +0330

    adding try catch

--- Services/ShopService/ShopService.Application/Services/RabbitMq/RabbitMqAppService.cs
+++ Services/ShopService/ShopService.Application/Services/RabbitMq/RabbitMqAppService.cs
@@ -1,4 +1,5 @@
 ﻿using LogService;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using ShopService.ApplicationContract.Interfaces.RabbitMq;
@@ -10,45 +11,64 @@ namespace ShopService.Application.Services.RabbitMq
     public class RabbitMqAppService : IRabbitMqAppService
     {
         private readonly ILogAppService _logAppService;
+        private readonly ILogger<RabbitMqAppService> _logger;
 
-        public RabbitMqAppService(ILogAppService logAppService)
+        public RabbitMqAppService(ILogAppService logAppService,ILogger<RabbitMqAppService> logger)
         {
             _logAppService = logAppService;
+            _logger = logger;
         }
         public async Task GetMessage<T>(CancellationToken cancellationToken = default)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = await factory.CreateConnectionAsync();
-            using var channel = await connection.CreateChannelAsync();
-            await channel.ExchangeDeclareAsync(exchange: "productService", type: ExchangeType.Direct, durable: true, autoDelete: false, arguments: null);
-            await channel.QueueDeclareAsync(queue: "productServiceQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-            await channel.QueueBindAsync(queue: "productServiceQueue", exchange: "productService", routingKey: "productServiceRoutingKey", arguments: null);
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.ReceivedAsync += async (model, ea) =>
+            try
             {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                var data = JsonSerializer.Deserialize<T>(message);
-                await _logAppService.LogAsync(message, nameof(RabbitMqAppService), "e681b47e-4c97-4a00-93fa-11c64fb93c41");
-                await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
-            };
-            await channel.BasicConsumeAsync(queue: "productServiceQueue", autoAck: false, consumer: consumer);
-            await Task.Delay(Timeout.Infinite, cancellationToken);
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using var connection = await factory.CreateConnectionAsync();
+                using var channel = await connection.CreateChannelAsync();
+                await channel.ExchangeDeclareAsync(exchange: "productService", type: ExchangeType.Direct, durable: true, autoDelete: false, arguments: null);
+                await channel.QueueDeclareAsync(queue: "productServiceQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                await channel.QueueBindAsync(queue: "productServiceQueue", exchange: "productService", routingKey: "productServiceRoutingKey", arguments: null);
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.ReceivedAsync += async (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var data = JsonSerializer.Deserialize<T>(message);
+                    await _logAppService.LogAsync(message, nameof(RabbitMqAppService), "e681b47e-4c97-4a00-93fa-11c64fb93c41");
+                    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                await channel.BasicConsumeAsync(queue: "productServiceQueue", autoAck: false, consumer: consumer);
+                await Task.Delay(Timeout.Infinite, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("There is problem with consumer side..");
+            }
+           
         }
 
         public async Task PublishMessage<T>(T value)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = await factory.CreateConnectionAsync();
-            using var channel = await connection.CreateChannelAsync();
-            await channel.ExchangeDeclareAsync(exchange: "productService", type: ExchangeType.Direct, durable: true, autoDelete: false, arguments: null);
-            await channel.QueueDeclareAsync(queue: "productServiceQueue",durable:true,exclusive:false,autoDelete:false,arguments:null);
-            await channel.QueueBindAsync(queue: "productServiceQueue", exchange: "productService", routingKey: "productServiceRoutingKey", arguments: null);
-            var messageBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value));
-            var properties = new BasicProperties { Persistent = true };
-            await channel.BasicPublishAsync(exchange: "productService", routingKey: "productServiceRoutingKey", mandatory:true,basicProperties: properties, body: messageBody);
-            await channel.CloseAsync();
-            await connection.CloseAsync();
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using var connection = await factory.CreateConnectionAsync();
+                using var channel = await connection.CreateChannelAsync();
+                await channel.ExchangeDeclareAsync(exchange: "productService", type: ExchangeType.Direct, durable: true, autoDelete: false, arguments: null);
+                await channel.QueueDeclareAsync(queue: "productServiceQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                await channel.QueueBindAsync(queue: "productServiceQueue", exchange: "productService", routingKey: "productServiceRoutingKey", arguments: null);
+                var messageBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value));
+                var properties = new BasicProperties { Persistent = true };
+                await channel.BasicPublishAsync(exchange: "productService", routingKey: "productServiceRoutingKey", mandatory: true, basicProperties: properties, body: messageBody);
+                await channel.CloseAsync();
+                await connection.CloseAsync();
+
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError("There is problem with publishing side..");
+            }
+            
         }
     }
 }
